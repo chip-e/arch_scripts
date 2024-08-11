32c2f1a020122547be4c330d77fabc1e5917ae53
Chi P. <chip.e.98@protonmail.com>
2024-08-11 01:52:44 -0500

    Updated hwfetch.sh

--- hwfetch.sh
+++ hwfetch.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 set -euo pipefail
+# Displays Systems and hardware info
+# WIP
 
-lscpu | grep "Model name:"
-lscpu | grep "Core(s)"
+# kernel and system architecture
+kernel=$(uname -a | awk {'print $2 " "  $NF'})
+echo "Kernel: $kernel"
+
+# distro
+# distro=$(head -n1 /etc/issue)
+# distro=$(cat /etc/*-release | grep "NAME" | awk {'print $2'})
+distro=$(cat /etc/*-release | grep "NAME" 
+echo "Distribution: $distro"
+# cpu info
+cpuname=$(lscpu | grep "Model name:")
+cpucores=$(lscpu | grep "Core(s)")
+# ram
+ram=$(grep MemTotal /proc/meminfo)
+
